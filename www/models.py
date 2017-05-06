9566348712fb56a470da3cecf2cb627b79ee2159
1966366130 <1966366130@qq.com>
2017-05-06 23:58:50 +0800

    04 day

--- www/models.py
+++ www/models.py
@@ -7,7 +7,7 @@ from orm import Model,StringField,BooleanField,FloatField,TextField
 def next_id():
 	return '%015d%s000' % (int(time.time()*1000),uuid.uuid4().hex)
 class User(Model):
-	__table__=='users'
+	__table__='users'
 
 	id=StringField(primary_key=True,default=next_id,ddl='varchar(50)')
 	email=StringField(ddl='varchar(50)')
