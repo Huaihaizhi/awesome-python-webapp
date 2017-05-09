79ace4d53e023f569ad10d096eb50fc97c6bf823
1966366130 <1966366130@qq.com>
2017-05-09 23:07:43 +0800

    some changes

--- www/testorm.py
+++ www/testorm.py
@@ -9,14 +9,11 @@ loop=asyncio.get_event_loop()
 def test():
     yield from orm.create_pool(loop,user='www-data', password='www-data', database='awesome')
 
-    u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
 
     yield from u.save()
-    r=yield from User.findAll()
-    print(r)
- #   yield from orm.destory_pool()
 
-for x in test():
-    pass
+#for x in test():
+#    pass
 loop.run_until_complete(test())
 loop.close()
