f37242c96fd9620133ce78cbbe897d439c30aae6
1966366130 <1966366130@qq.com>
2017-05-13 23:24:47 +0800

    some changes

--- www/app.py
+++ www/app.py
@@ -5,18 +5,113 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment,FileSystemLoader
+import orm
+from coroweb import add_routes,add_static
 
-async def index(request):
-	return web.Response(content_type='text/html',body=b'<h1>Awesome</h1>')
+def init_jinja2(app,**kw):
+	logging.info('init jinja2...')
+	options=dict(
+		autoescape=kw.get('autoescape',True),
+		block_start_string=kw.get('block_start_string','{%'),
+		block_end_string=kw.get('block_end_string','%}'),
+		variable_start_string=kw.get('variable_start_string','{{%'),
+		variable_end_string=kw.get('variable_end_string','%}}'),
+		auto_reload=kw.get('auto_reload',True)
+	)
+	path=kw.get('path',None)
+	if path is None:
+		path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+	logging.info('set jinja2 template path:%s' % path)
+	env=Environment(loader=FileSystemLoader(path),**options)
+	filters=kw.get('filters',None)
+	if filters is not None:
+		for name,f in filters.items():
+			env.filters[name]=f
+	app['__templating__']=env
 
+async def logger_factory(app,handler):
+	async def logger(request):
+		logging.info('Request:%s%s' % (request.method,request.path))
+		return (await handler(request))
+	return logger
+
+async def data_factory(app,handler):
+	async def parse_data(request):
+		if request.method=='POST':
+			if request.content_type.startwith('appliation/json'):
+				request.__data__=await request.json()
+				logging.info('request json:%s' % str(request.__data__))
+			elif request.content_type.startwith('application/x-www-form-urlencoded'):
+				request.__data__=await request.post()
+				logging.info('request form:%s' % str(request.__data__))
+		return (await handler(request))
+	return parse_data
+
+async def response_factory(app,handler):
+	async def response(request):
+		logging.info('Response hanler...')
+		r=await handler(request)
+		if isinstance(r,web.StreamResponse):
+			return r
+		if isinstance(r,bytes):
+			resp=web.Response(body=r)
+			resp=content_type='application/octet-stream'
+			return resp
+		if isinstance(r,str):
+			if r.startwith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp=web.Response(body=r.encode('utf-8'))
+			resp.content_type='text/html;charset=utf-8'
+			return resp
+		if isinstance(r,dict):
+			template=r.get('__template__')
+			if template is None:
+				res=web.Response(Body=json.dumps(r,ensure_ascii=False,default=lambda o: o.__dict__).encode('utf-8'))
+				res.content_type='application/json;charset=utf-8'
+				return resp
+			else:
+				res=web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				res.content_type='text/html;charset=utf-8'
+				return resp
+		if isinstance(r,int) and r >=100 and r<600:
+			return web.Response(r)
+		if isinstance(r,tuple) and len(r)==2:
+			t,m=r
+			if isinstance(t,int) and t>=100 and t<=600:
+				return web.Response(t,str(m))
+		resp=web.Response(body=str(r).encode('utf-8'))
+		resp.content_type='text/plain;charset=utf-8'
+		return resp
+	return response
+
+def datetime_filter(t):
+	delta=int(time.time() - t)
+	if delta<60:
+		return u'1分钟前'
+	if delta<3600:
+		return u'%s分钟前' % (delta//60)
+	if delta<86400:
+		return u'%s小时前' % (delta//3600)
+	if delta<604800:
+		return u'%s天前' % (delta//86400)
+	dt=datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year,dt.month,dt.day)
+
+			
+loop=asyncio.get_event_loop()
 @asyncio.coroutine
 def init(loop):
-	app=web.Application(loop=loop)
-	app.router.add_route('GET','/',index)
+	yield from orm.create_pool(loop,user='www',password='www',database='awesome')
+	app=web.Application(loop=loop,middlewares=[
+		logger_factory,response_factory
+	])
+	init_jinja2(app,filter=dict(datetime=datetime_filter))
+	add_routes(app,'handlers')
+	add_static(app)
 	srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
-	logging.info('Server started at http://127.0.0.1:8000...')
+	logging.info('Server started at http://127.0.0.1:9000...')
 	return srv
 
-loop=asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
--- www/testorm.py
+++ www/testorm.py
@@ -10,8 +10,9 @@ def test():
     yield from orm.create_pool(loop,user='www-data', password='www-data', database='awesome')
 
     u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-
+    m = User(name='Test1', email='test1@example.com', passwd='0987654321', image='about:blank')
     yield from u.save()
+    yield from m.save()
 
 #for x in test():
 #    pass
