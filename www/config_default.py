8d166b7fb0beceeb75d638ff952e1f7ca001f7dc
1966366130 <1966366130@qq.com>
2017-05-14 22:48:25 +0800

    06 day

---
+++ www/config_default.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+configs={
+	'debug':True,
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'www-data',
+		'password':'www-data',
+		'db':'awesome'
+	},
+	'session':{
+		'secret':'Awesome'
+	}
+}
