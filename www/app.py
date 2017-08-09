3675124f07b8eced186e9e4ebddbd7ccac7897bd
1966366130 <1966366130@qq.com>
2017-08-09 14:51:20 +0800

    some changes

--- www/app.py
+++ www/app.py
@@ -72,43 +72,45 @@ def data_factory(app,handler):
 	return parse_data
 
 @asyncio.coroutine
-def response_factory(app,handler):
-	@asyncio.coroutine
-	def response(request):
-		logging.info('Response hanler...')
-		r=yield from handler(request)
-		if isinstance(r,web.StreamResponse):
-			return r
-		if isinstance(r,bytes):
-			resp=web.Response(body=r)
-			resp=content_type='application/octet-stream'
-			return resp
-		if isinstance(r,str):
-			if r.startwith('redirect:'):
-				return web.HTTPFound(r[9:])
-			resp=web.Response(body=r.encode('utf-8'))
-			resp.content_type='text/html;charset=utf-8'
-			return resp
-		if isinstance(r,dict):
-			template=r.get('__template__')
-			if template is None:
-				resp=web.Response(body=json.dumps(r,ensure_ascii=False,default=lambda o: o.__dict__).encode('utf-8'))
-				resp.content_type='application/json;charset=utf-8'
-				return resp
-			else:
-				resp=web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
-				resp.content_type='text/html;charset=utf-8'
-				return resp
-		if isinstance(r,int) and r >=100 and r<600:
-			return web.Response(r)
-		if isinstance(r,tuple) and len(r)==2:
-			t,m=r
-			if isinstance(t,int) and t>=100 and t<=600:
-				return web.Response(t,str(m))
-		resp=web.Response(body=str(r).encode('utf-8'))
-		resp.content_type='text/plain;charset=utf-8'
-		return resp
-	return response
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
+        logging.info('Response handler...')
+        r = yield from handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                r['__user__'] = request.__user__
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and t >= 100 and t < 600:
+            return web.Response(t)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
 
 def datetime_filter(t):
 	delta=int(time.time() - t)
